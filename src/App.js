1238f7c592c041ee0dc9c0910c8968ae727a60d8
Traci <traci.kamp@blackburn.edu>
2018-09-29 12:04:15 -0500

    Build the layout of the main app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container className="App">
+        <Row>
+
+          {/* List Column */}
+          <Col xs="3"></Col>
+
+          {/* Map Column */}
+          <Col>
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
