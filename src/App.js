10acd23a1a21b23911409ae3b5e10eac27384220
Traci <traci.kamp@blackburn.edu>
2018-10-02 20:50:52 -0500

    Make the container fluid

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Container className="App">
+      <Container fluid className="App">
         <Header />
 
         <Row>
