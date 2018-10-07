bac0d765f7fc27abe6e495c85b32f6a362fe70a6
Traci <traci.kamp@blackburn.edu>
2018-10-06 20:17:55 -0500

    Add all of the attractions visited in iceland

--- src/App.js
+++ src/App.js
@@ -7,11 +7,49 @@ import './App.css';
 
 class App extends Component {
   state = {
-    locations: []
-  }
-
-  componentDidMount() {
-
+    // In lieu of hosting an express server, just hard code the locations.
+    locations: [
+      {
+        name: 'Viking World',
+        coordinates: { lat: 63.9759731, lng: -22.5311803 }
+      },
+      {
+        name: 'Grand Hótel Reykjavík',
+        coordinates: { lat: 64.1423523, lng: -21.8937487 }
+      },
+      {
+        name: 'Hallgrímskirkja',
+        coordinates: { lat: 64.1417172, lng: -21.9288257 }
+      },
+      {
+        name: 'Brú Guesthouse',
+        coordinates: { lat: 63.6658746, lng: -20.0476 },
+      },
+      {
+        name: 'Skógafoss',
+        coordinates: { lat: 63.5320147, lng: -19.513565 }
+      },
+      {
+        name: 'Sólheimasandur Plane Wreck',
+        coordinates: { lat: 63.4590942, lng: -19.3669009 }
+      },
+      {
+        name: 'Smi∂jan Brugghús',
+        coordinates: { lat: 63.4172624, lng: -19.012781 }
+      },
+      {
+        name: 'Vatnajökull Glacier',
+        coordinates: { lat: 64.4094659, lng: -17.5283124 }
+      },
+      {
+        name: 'Svartifoss',
+        coordinates: { lat: 64.0275306, lng: -16.9774969 }
+      },
+      {
+        name: 'Hof 1 Hotel',
+        coordinates: { lat: 63.9070156, lng: -16.708245 }
+      }
+    ]
   }
 
   render() {
@@ -25,7 +63,7 @@ class App extends Component {
           </Col>
 
           <Col>
-            <GoogleMap id={'map'} />
+            <GoogleMap id={'map'} locations={this.state.locations} />
           </Col>
         </Row>
       </Container>
