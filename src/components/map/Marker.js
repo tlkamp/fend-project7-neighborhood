d3be8eb96b21f8156ecd534019463d366bf37a5d
Traci <traci.kamp@blackburn.edu>
2018-10-08 21:01:47 -0500

    Turn them blue on hover

--- src/components/map/Marker.js
+++ src/components/map/Marker.js
@@ -4,7 +4,7 @@ class Marker extends Component {
   render() {
     return (
       <svg margin-top="-1000" margin-left="-1000" height="15" width="15">
-        <circle cx="7" cy="7" r="5" stroke="white" strokeWidth="1" fill="red" />
+        <circle cx="7" cy="7" r="5" stroke="white" strokeWidth="1" fill={this.props.$hover ? "blue" : "red"} />
       </svg>
     );
   }
