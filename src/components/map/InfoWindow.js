edb954587f60c15c82c6fe86b2acdb9e4d7878b5
Traci <traci.kamp@blackburn.edu>
2018-10-14 19:38:58 -0500

    Remove unused button import

--- src/components/map/InfoWindow.js
+++ src/components/map/InfoWindow.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import './InfoWindow.css';
 
 class InfoWindow extends Component {
