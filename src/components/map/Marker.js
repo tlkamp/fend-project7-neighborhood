0dce46b6a101a764ed870a862753a413e8c58dd2
Traci <traci.kamp@blackburn.edu>
2018-10-20 10:54:47 -0500

    tabindex -> tabIndex

--- src/components/map/Marker.js
+++ src/components/map/Marker.js
@@ -15,7 +15,7 @@ class Marker extends Component {
 
   render() {
     return (
-      <div tabindex="0" onKeyDown={(event) => this.onKeyDown(event.keyCode, this.props.location)}>
+      <div tabIndex="0" onKeyDown={(event) => this.onKeyDown(event.keyCode, this.props.location)}>
         <svg onClick={() => this.props.onMarkerClick(this.props.location)} margin-top="-1000" margin-left="-1000" height="15" width="15">
           <circle cx="7" cy="7" r="5" stroke="white" strokeWidth="1" />
         </svg>
