31cfc8d1bcc532d2b25f4eb433bcd2bfb4dee4df
Traci <traci.kamp@blackburn.edu>
2018-10-20 10:41:13 -0500

    Add the locations and markers to the tab order

--- src/components/map/Marker.js
+++ src/components/map/Marker.js
@@ -12,7 +12,7 @@ class Marker extends Component {
 
   render() {
     return (
-      <div>
+      <div tabindex="0">
         <svg onClick={() => this.props.onMarkerClick(this.props.location)} margin-top="-1000" margin-left="-1000" height="15" width="15">
           <circle cx="7" cy="7" r="5" stroke="white" strokeWidth="1" />
         </svg>
