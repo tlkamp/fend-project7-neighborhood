6b2f9e5713a1ee6b7118ed99c455f4048fb39524
Traci <traci.kamp@blackburn.edu>
2018-10-20 11:00:49 -0500

    Change the color of the marker when it is clicked

--- src/components/map/Marker.js
+++ src/components/map/Marker.js
@@ -17,7 +17,7 @@ class Marker extends Component {
     return (
       <div tabIndex="0" onKeyDown={(event) => this.onKeyDown(event.keyCode, this.props.location)}>
         <svg onClick={() => this.props.onMarkerClick(this.props.location)} margin-top="-1000" margin-left="-1000" height="15" width="15">
-          <circle cx="7" cy="7" r="5" stroke="white" strokeWidth="1" />
+          <circle cx="7" cy="7" r="5" stroke="white" strokeWidth="1" fill={this.props.location.active ? 'blue' : 'red'} />
         </svg>
 
         {this.props.location.active ? (<InfoWindow location={this.props.location} />) : null}
