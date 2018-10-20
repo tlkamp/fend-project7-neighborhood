7c8da1165f6187324f0b01937b097d1fc3fb0749
Traci <traci.kamp@blackburn.edu>
2018-10-20 14:46:54 -0500

    Render the photos on demand and show an alert if there's an issue

--- src/components/map/InfoWindow.js
+++ src/components/map/InfoWindow.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Alert } from 'reactstrap';
 import './InfoWindow.css';
 
 class InfoWindow extends Component {
+
+  state = {
+    url: undefined,
+    error: false
+  }
+
+  componentDidMount() {
+    const unsplashClient = 'bd51d15e794da625807ddcb09eb619db5f26e44f4a23ece2415657b20899c7aa';
+    fetch(`https://api.unsplash.com/photos/${this.props.location.photoid}/?w=140&h=100`, {
+      headers: {
+        'Authorization': `Client-ID ${unsplashClient}`
+      }
+    }
+    )
+      .then(response => response.json())
+      .then(data => this.setState({ url: data.urls.small }))
+      .catch(error => { this.setState({ error: true }) });
+  }
 
   render() {
     return (
       <div className="info-window">
         <Card>
-          <CardImg top src={this.props.location.photoUrl} alt={this.props.location.name} />
+          {this.state.error ? <Alert color="warning">This resource could not be loaded.</Alert> : <CardImg top src={this.state.url} alt={this.props.location.name} />}
           <CardBody>
             <CardTitle>{this.props.location.name}</CardTitle>
             <CardText>{this.props.location.description}</CardText>
