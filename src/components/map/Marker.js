3592f95e0349dda9ff3735ba6ef14e9c5f4bf1eb
Traci <traci.kamp@blackburn.edu>
2018-10-20 10:54:22 -0500

    Handle enter and space like a click

--- src/components/map/Marker.js
+++ src/components/map/Marker.js
@@ -4,15 +4,18 @@ import './Marker.css';
 
 class Marker extends Component {
 
-  onClick = () => {
-    this.setState((previousState) => {
-      return { infoWindowShowing: !previousState.infoWindowShowing }
-    });
+  onKeyDown = (keyCode, location) => {
+    // if the key is enter or space
+    if (keyCode === 13 || keyCode === 32) {
+      if (this.props.onMarkerClick) {
+        this.props.onMarkerClick(location);
+      }
+    }
   }
 
   render() {
     return (
-      <div tabindex="0">
+      <div tabindex="0" onKeyDown={(event) => this.onKeyDown(event.keyCode, this.props.location)}>
         <svg onClick={() => this.props.onMarkerClick(this.props.location)} margin-top="-1000" margin-left="-1000" height="15" width="15">
           <circle cx="7" cy="7" r="5" stroke="white" strokeWidth="1" />
         </svg>
